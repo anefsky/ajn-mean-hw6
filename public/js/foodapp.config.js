8e0b1a0ce16814530c4d6d22a9f3836e8a268e69
Andrew Nefsky <anefsky@gmail.com>
2017-03-12 16:08:06 -0400

    added animation between pages

--- public/js/foodapp.config.js
+++ public/js/foodapp.config.js
@@ -2,16 +2,19 @@ angular.module('foodApp').config(
 	function($routeProvider, $locationProvider){
 		$routeProvider.
 		when('/', {
-			template: 'test'
+			template: 'test',
+			controller: 'HomeCtrl'
 		}).
 		when('/recipes', {
-			template: '<recipe-list></recipe-list>'
+			template: '<recipe-list></recipe-list>',
+			controller: 'RecipeCtrl'
 		}).
 		when('/recipes/:recipeId', {
 			template: '<recipe-detail></recipe-detail>'
 		}).
 		when('/reviews', {
-			template: '<reviews-list></reviews-list>'
+			template: '<reviews-list></reviews-list>',
+			controller: 'ReviewCtrl'
 		}).
 		otherwise({
 			redirectTo: '/404'
--- public/js/foodapp.module.js
+++ public/js/foodapp.module.js
@@ -1,1 +1,16 @@
-var app = angular.module('foodApp', ['ngRoute']);
+var app = angular.module('foodApp', ['ngRoute', 'ngAnimate']);
+
+app.controller('HomeCtrl', ['$scope',
+    function MainCtrl($scope) {
+    	$scope.pageClass = 'page-home';
+    }]);
+
+app.controller('RecipeCtrl', ['$scope',
+    function MainCtrl($scope) {
+    	$scope.pageClass = 'page-recipe';
+    }]);
+
+app.controller('ReviewCtrl', ['$scope',
+    function MainCtrl($scope) {
+    	$scope.pageClass = 'page-review';
+    }]);
