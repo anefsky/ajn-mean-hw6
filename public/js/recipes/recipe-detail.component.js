0b0749e2bfc9bb2d5d7406b85ae69ac60288166e
Andrew Nefsky <anefsky@gmail.com>
2017-03-12 21:13:19 -0400

    Added hover for detail

--- public/js/recipes/recipe-detail.component.js
+++ public/js/recipes/recipe-detail.component.js
@@ -2,12 +2,24 @@ angular.module('foodApp').component('recipeDetail', {
 	templateUrl: 'js/recipes/recipe-detail.template.html',
 	controller: function RecipeDetailController($http, $routeParams) {
 		
-	    $http.get('data/' + $routeParams.recipeId +  '.json')
-	      .then( response => {
-	      	this.recipe = response.data;
-	      	this.setImage(this.recipe.images[0]);
-	    });
+		$http.get('data/' + $routeParams.recipeId +  '.json')
+		.then( response => {
+			this.recipe = response.data;
+			this.setImage(this.recipe.images[0]);
+			this.handleImageHighlighter();
+		});
 
 		this.setImage = imageUrl => this.mainImageUrl = imageUrl;
+
+		this.handleImageHighlighter = function() {
+			setTimeout(() => {  // needs time to render
+				const imagesList = document.querySelectorAll('.recipe-thumbs li img');
+				imagesList.forEach(image => image.addEventListener('mouseenter', highlightLink));
+				const highlight = document.createElement('span');
+				highlight.classList.add('highlight');
+				document.body.append(highlight);
+			}, 1000);  
+		}
+
 	}
 })
